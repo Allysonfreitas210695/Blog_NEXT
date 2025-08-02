f381030716fed21ea8029e031d71e7fcbb6918bd
Allyson Freitas <alisonfr83@gmail.com>
2025-08-02 09:35:25 -0300

    feat: Usando SSG para fazer o build

--- src/templates/landingPage/index.tsx
+++ src/templates/landingPage/index.tsx
@@ -1,5 +1,4 @@
-import { CallToAction } from "@/templates/landingPage/section/call-to-action";
-import { CustomerStorySection } from "@/templates/landingPage/section/customer-story-section";
+ import { CustomerStorySection } from "@/templates/landingPage/section/customer-story-section";
 import { FeatureSection } from "@/templates/landingPage/section/feature-section";
 import { HeroSection } from "@/templates/landingPage/section/hero-section";
 import { SupportSection } from "@/templates/landingPage/section/support-section";
@@ -10,8 +9,7 @@ export const LandingPage = () => {
       <HeroSection />
       <FeatureSection />
       <SupportSection />
-      <CustomerStorySection />
-      <CallToAction />
+      <CustomerStorySection /> 
     </article>
   );
 };
